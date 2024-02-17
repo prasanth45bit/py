65fbad1052a192232f64a367d5b50579745d6150
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-02-17 19:31:58 +0530

    Login.py

--- Login.py
+++ Login.py
@@ -1,13 +1,9 @@
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    
-    # Check if username and password match a predefined set of credentials
-    if username == "admin" and password == "password":
+       if username == "admin" and password == "password":
         print("Login successful!")
     else:
         print("Incorrect username or password. Please try again.")
-
-# Main function to execute the login program
 if __name__ == "__main__":
     login()
