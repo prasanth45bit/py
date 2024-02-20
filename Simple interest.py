8813054933b7e47236c47895ac7dec2081f8b53d
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-02-20 13:27:01 +0530

    Simple interest.py

--- Simple interest.py
+++ Simple interest.py
@@ -1,11 +1,8 @@
 def calculate_simple_interest(principal, rate, time):
     simple_interest = (principal * rate * time) / 100
     return simple_interest
-
-# Example usage:
 principal_amount = 1000
 interest_rate = 5
 time_period = 2
-
 result = calculate_simple_interest(principal_amount, interest_rate, time_period)
 print(f"The Simple Interest is: {result}")
